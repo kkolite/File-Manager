ea01fa37c12a1f2bfc76e1348c5ce48078096837
Yahor Litavar <yahor.litavar@softnetix.io>
2023-06-20 14:24:06 +0300

    feat: os username

--- src/script/os.js
+++ src/script/os.js
@@ -2,13 +2,18 @@ import os from 'os';
 
 const optionList = {
     cpus: '--cpus',
+    username: '--username',
 }
 
 const getOsInfo = (option) => {
-    const cpus = os.cpus();
-
     if (option.match(optionList.cpus)) {
+        const cpus = os.cpus();
         const message = `Total: ${cpus.length} \n ${cpus.reduce((acc, cpu) => acc + `Name: ${cpu.model}\nClock rate: ${cpu.speed}GHz\n\n`, '')}`
+        console.log(message)
+    }
+
+    if (option.match(optionList.username)) {
+        const message = os.userInfo().username;
         console.log(message)
     }
 }
