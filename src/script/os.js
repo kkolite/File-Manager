da0f2b5e4ed4c67816331e1b696c990618eb974d
Yahor Litavar <yahor.litavar@softnetix.io>
2023-06-20 14:39:06 +0300

    feat: os error case

--- src/script/os.js
+++ src/script/os.js
@@ -1,5 +1,7 @@
 import os from 'os';
 
+const { stdout } = process;
+
 const optionList = {
     cpus: '--cpus',
     username: '--username',
@@ -12,28 +14,36 @@ const getOsInfo = (option) => {
     if (option.match(optionList.cpus)) {
         const cpus = os.cpus();
         const message = `Total: ${cpus.length} \n ${cpus.reduce((acc, cpu) => acc + `Name: ${cpu.model}\nClock rate: ${cpu.speed}GHz\n\n`, '')}`
-        console.log(message)
+        stdout.write(message);
+        return;
     }
 
     if (option.match(optionList.username)) {
         const message = os.userInfo().username;
-        console.log(message)
+        stdout.write(message);
+        return;
     }
 
     if (option.match(optionList.homedir)) {
         const message = os.userInfo().homedir;
-        console.log(message)
+        stdout.write(message);
+        return;
     }
 
     if (option.match(optionList.EOL)) {
         const message = os.EOL;
-        console.log(JSON.stringify(message))
+        stdout.write(JSON.stringify(message));
+        return;
     }
 
     if (option.match(optionList.arch)) {
         const message = os.arch();
-        console.log(message)
+        stdout.write(message);
+        return;
     }
+
+    const errorMessage = `Incorrect command.\nAvailable commands:\n${[Object.values(optionList).join(`\n`)]}\n`;
+    stdout.write(errorMessage);
 }
 
 export { getOsInfo }
